4e3494ce7bc53c73eaddb2ce9b52be18d65f1f5c
laogao <871593435@qq.com>
2020-06-21 08:35:28 +0800

    修改了一下小问题

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,4 +11,34 @@ $(function () {
         $('.red-box').show();
         $('.login-box').hide();
     });
+    //从layui 里面获取到 layui -form
+    var form = layui.form;
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        // 注册两次密码 验证
+        repwd: function (value) {
+            //获取第一次密码框的值 
+            var pwd = $('.login-box [name=password]').val().trim();
+            //判断两个值是否相同
+            if (pwd !== value) return '密码不相同,请重新输入!'
+        }
+    });
+    //监听 表单注册事件
+    $('#form_reg').on('submit', function (e) {
+        //阻止表单的默认跳转行为
+        e.prevenDefault();
+        console.log('333');
+        //发送ajax请求
+        $.post('http://ajax.frontend.itheima.net/api/reguser', {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }, function (res) {
+            console.log(res);
+            if (res.status !== 0) return alert(res.message);
+            console.log(res.message);
+        });
+    });
 });
